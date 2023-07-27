9c831b643eb7b6743deedc3c8dc6292e8e5f2bf9
Aquerr <nerdianin@gmail.com>
2023-07-27 09:55:46 +0200

    Refactor getting profile for current user.

--- src/main/java/io/github/aquerr/rentacar/domain/profile/ProfileService.java
+++ src/main/java/io/github/aquerr/rentacar/domain/profile/ProfileService.java
@@ -2,15 +2,14 @@ package io.github.aquerr.rentacar.domain.profile;
 
 import io.github.aquerr.rentacar.domain.profile.converter.ProfileConverter;
 import io.github.aquerr.rentacar.domain.profile.dto.UserProfileDto;
+import io.github.aquerr.rentacar.domain.profile.exception.ProfileNotFoundException;
 import io.github.aquerr.rentacar.repository.ProfileRepository;
-import io.github.aquerr.rentacar.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -18,26 +17,18 @@ public class ProfileService {
 
     private final ProfileRepository profileRepository;
     private final ProfileConverter profileConverter;
-    private final UserRepository userRepository;
-
-    @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
-    public UserProfileDto getProfileByUsername(String username)
-    {
-        long credentialsId = userRepository.findByUsername(username).getId();
-        return profileConverter.toProfileDto(profileRepository.findByCredentialsId(credentialsId));
-    }
 
     @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
     public List<UserProfileDto> getProfiles() {
         return profileRepository.findAll().stream()
                 .map(profileConverter::toProfileDto)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
     public UserProfileDto getProfileById(Long id) {
         return profileRepository.findById(id)
                 .map(profileConverter::toProfileDto)
-                .orElse(null);
+                .orElseThrow(ProfileNotFoundException::new);
     }
 }
