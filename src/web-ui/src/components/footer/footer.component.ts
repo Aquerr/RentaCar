14353f82eb6401f2f0a8eea5d43a8a2ae1c38249
Aquerr <nerdianin@gmail.com>
2023-08-03 21:14:54 +0200

    Wstępny szablon aplikacji

---
+++ src/web-ui/src/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+
+export class FooterComponent {
+  year:number = new Date().getFullYear();
+}
