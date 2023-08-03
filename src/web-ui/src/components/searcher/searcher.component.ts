379807bdd04cae64c31adddc8a1f4f76c181e9a5
Mateusz <mtpala@interia.pl>
2023-08-03 10:41:01 +0200

    Dodano zaczytywanie miejsc odbioru do dropdown'a

--- src/web-ui/src/components/searcher/searcher.component.ts
+++ src/web-ui/src/components/searcher/searcher.component.ts
@@ -1,24 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PickupLocationApiService } from '../../services/api/pickup-location-api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'searcher',
   templateUrl: './searcher.component.html',
-  styleUrls: ['./searcher.component.scss'],
+  styleUrls: ['./searcher.component.scss']
 })
-export class SearcherComponent implements OnInit {
+export class SearcherComponent implements OnInit, OnDestroy {
+  subscription = new Subscription();
   dateFrom = new Date();
   hourFrom = new Date();
   dateTo = new Date;
   hourTo = new Date();
-  places= [{name: 'Warszawa', code: 'WW'},
-    {name: 'Gdańsk', code: 'GD'}];
-  selectedPlace: any;
+  pickupLocations: PickupLocationDropdown[] = [];
+  selectedPickupLocation: PickupLocationDropdown | null = null;
 
-  constructor() {
+  constructor(private apiService: PickupLocationApiService) {
   }
 
   ngOnInit() {
     this.setStartDates();
+    this.startApiServiceSubscription();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  startApiServiceSubscription() {
+    this.subscription = this.apiService.getPickupLocations().subscribe({
+      next: (response) => {
+        response.locations.sort((a, b) => a.city.localeCompare(b.city))
+        .forEach((location) => {
+          this.pickupLocations.push({
+            id: location.id,
+            displayName: location.name + ' (' + location.city + ')'
+          });
+        });
+      }
+    });
   }
 
   setStartDates() {
@@ -28,4 +49,9 @@ export class SearcherComponent implements OnInit {
     this.dateTo.setMinutes(0);
     this.hourTo.setMinutes(0);
   }
+}
+
+export interface PickupLocationDropdown {
+  id: number;
+  displayName: string;
 }
