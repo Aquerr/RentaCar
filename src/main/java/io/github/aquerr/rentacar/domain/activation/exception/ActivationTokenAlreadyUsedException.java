6f4e73de99e8ec50005b8b5b03c0870dd44b464b
Aquerr <nerdianin@gmail.com>
2023-08-06 10:02:49 +0200

    Drop UNIQUE property for credentials_id in account_acitvation_token table. Fix missing message keys.

--- src/main/java/io/github/aquerr/rentacar/domain/activation/exception/ActivationTokenAlreadyUsedException.java
+++ src/main/java/io/github/aquerr/rentacar/domain/activation/exception/ActivationTokenAlreadyUsedException.java
@@ -3,7 +3,7 @@ package io.github.aquerr.rentacar.domain.activation.exception;
 import io.github.aquerr.rentacar.domain.ApiException;
 import org.springframework.http.HttpStatus;
 
-@ApiException(status = HttpStatus.BAD_REQUEST, messageKey = "")
+@ApiException(status = HttpStatus.BAD_REQUEST, messageKey = "activation-token.error.already-used")
 public class ActivationTokenAlreadyUsedException extends RuntimeException
 {
 }
