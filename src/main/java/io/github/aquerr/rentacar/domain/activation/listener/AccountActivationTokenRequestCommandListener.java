68b00c7fc9ed23279d94c473e4e4f2665f5b6d84
Aquerr <nerdianin@gmail.com>
2023-11-30 11:34:32 +0100

    Implementation for password reset + cleanups

--- src/main/java/io/github/aquerr/rentacar/domain/activation/listener/AccountActivationTokenRequestCommandListener.java
+++ src/main/java/io/github/aquerr/rentacar/domain/activation/listener/AccountActivationTokenRequestCommandListener.java
@@ -2,8 +2,8 @@ package io.github.aquerr.rentacar.domain.activation.listener;
 
 import io.github.aquerr.rentacar.application.mail.MailMessage;
 import io.github.aquerr.rentacar.application.mail.MailType;
+import io.github.aquerr.rentacar.application.rabbit.RabbitMessageSender;
 import io.github.aquerr.rentacar.domain.activation.AccountActivationService;
-import io.github.aquerr.rentacar.domain.activation.ActivationLinkMailSender;
 import io.github.aquerr.rentacar.domain.activation.command.AccountActivationTokenRequestCommand;
 import io.github.aquerr.rentacar.domain.activation.model.ActivationTokenEntity;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +19,7 @@ import java.util.Map;
 public class AccountActivationTokenRequestCommandListener
 {
     private final AccountActivationService accountActivationService;
-    private final ActivationLinkMailSender activationLinkMailSender;
+    private final RabbitMessageSender rabbitMessageSender;
 
     @RabbitListener(queues = "account.activation.request")
     public void process(AccountActivationTokenRequestCommand command)
@@ -28,12 +28,26 @@ public class AccountActivationTokenRequestCommandListener
 
         ActivationTokenEntity activationTokenEntity = accountActivationService.invalidateOldActivationTokensAndGenerateNew(command.getCredentialsId());
 
-        this.activationLinkMailSender.send(MailMessage.builder()
+        MailMessage mailMessage = MailMessage.builder()
                         .to(command.getEmailTo())
                         .subject("Account Activation")
                         .type(MailType.ACCOUNT_ACTIVATION)
                         .langCode(command.getLangCode())
                         .properties(Map.of("activation_token", activationTokenEntity.getToken()))
-                .build());
+                .build();
+        sendMail(mailMessage);
+    }
+
+    private void sendMail(MailMessage mailMessage)
+    {
+        try
+        {
+            rabbitMessageSender.send("mail.send", mailMessage);
+        }
+        catch (Exception e)
+        {
+            //TODO: We should retry...
+            log.error("Could not send message {}", mailMessage, e);
+        }
     }
 }
